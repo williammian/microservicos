70a60bfb9cdcf8a958426961156ef77071b4aa93
William Mian <william_mian@yahoo.com.br>
2021-04-14 13:36:06 -0300

    validando o token

--- fornecedorms/src/main/java/br/com/wm/microservice/fornecedorms/FornecedormsApplication.java
+++ fornecedorms/src/main/java/br/com/wm/microservice/fornecedorms/FornecedormsApplication.java
@@ -2,8 +2,10 @@ package br.com.wm.microservice.fornecedorms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class FornecedormsApplication {
 
 	public static void main(String[] args) {
